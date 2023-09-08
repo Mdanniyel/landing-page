04e97fb72b5ffd5ee91a79d6f891b2e4e3f3a0c9
Mdanniyel <levi.daniel.dl@gmail.com>
2023-09-08 13:14:25 +0300

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: 'class',
   content: ["./**/*.{html,js}", 'node_modules/preline/dist/*.js'],
   theme: {
+    fontFamily: {
+      heebo: ['Heebo', 'sans-serif'],
+      noto: ['Noto Sans Hebrew', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [require('preline/plugin')],
